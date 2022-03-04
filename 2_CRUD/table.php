9bf6b17e35d6e49b0a764b3317cef17e49be47e5
belajarApa <gantiapa00@gmail.com>
2022-03-04 21:02:17 +0700

    table

---
+++ 2_CRUD/table.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../bootstrap-5.1.3/css/bootstrap.min.css">
+    <title>CRUD</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-10" style="margin: auto;">
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col" class="number">Number</th>
+      <th scope="col">Image</th>
+      <th scope="col">Product</th>
+      <th scope="col">Price</th>
+      <th scope="col">Total</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row" class="num"></th>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    </div>
+</div>
+    <script src="../bootstrap-5.1.3/js/bootstrap.min.js"></script>
+</body>
+</html>
